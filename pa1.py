fd718645b914fbde1b4814133d176b4ab2e81111
HzaRashid <hamzarashid.mail@gmail.com>
2024-09-21 17:55:53 -0400

    misc

--- pa1.py
+++ pa1.py
@@ -20,5 +20,17 @@ fake_data = pd.read_csv('./data/fakes.txt', sep='\t')
 fake_data['is_fact'] = 0
 print(fake_data.head()) # verify
 
-''' tokenize '''
+# STORE DATA IN ONE TABLE
+data = pd.concat([fact_data, fake_data])
+# verify
+print(data.head()) # should be 1 along 'is_fact'
+print(data.tail()) # should be 0 along 'is_fact'
 
+
+''' 
+Preprocessing Decisions:
+- Convert text to lowercase, 
+- Remove all non-alphanumeric and non-whitespace characters
+- Tokenize (unigram)
+- Term frequency feature vector
+'''
